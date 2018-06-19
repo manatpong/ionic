cb394c0de4659a422c83c5cb1e68bc2f599a2d46
Manatpong <582110134@camt.info>
2018-06-19 16:42:14 +0700

    make a loop for display day

--- src/pages/plan/plan.ts
+++ src/pages/plan/plan.ts
@@ -1,3 +1,5 @@
+import { AngularFirestore } from 'angularfire2/firestore';
+import { AngularFireAuth } from 'angularfire2/auth';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
@@ -15,11 +17,37 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class PlanPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  items: any = [];
+  uid: string = '';
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public firebaseAuth: AngularFireAuth,
+    public firebaseFirestore: AngularFirestore
+  ) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PlanPage');
+    this.uid = this.firebaseAuth.auth.currentUser.uid;
+    
+    this.firebaseFirestore
+    .collection('plan')
+    .snapshotChanges()
+    // .valueChanges()
+    .subscribe((data:any) => {
+      this.items = [];
+
+      data.map(action => {
+        this.items.push({
+          id : action.payload.doc.id,
+          data : action.payload.doc.data(),
+        })
+        console.log(this.items);
+      });
+
+    })
   }
 
 }
